23bdbd17abda5c33ccfb116378744bec6e4027b0
sayphuvong <sayphuvong@gmail.com>
2018-07-31 07:28:36 +0700

    Project Creation

---
+++ source_code/src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="w-50 container">
+          <header>
+            <h1 className="font-weight-normal">ToDo App</h1>
+          </header>
+          <div className="app-form">
+            <div className="form-inline" id="task-form">
+              <input type="text" className="rounded-left" />
+              <button type="submit" className="btn rounded-right"><i className="fas fa-plus" /> Add Task</button>
+            </div>
+          </div>
+          <div className="todo-list">
+            <ul className="list-group w-75">
+              <li className="list-group-item position-relative active">
+                <p className="row">Cras justo odio Cras justo odio Cras justo odio Cras justo odio Cras justo odio Cras justo odio </p>
+                <div className="row tool position-absolute">
+                  <button className="bg-white"><i className="fas fa-pen" /></button>
+                  <button className="bg-primary"><i className="fas fa-spinner" /></button>
+                  <button className="bg-danger"><i className="fas fa-times" /></button>
+                </div>
+              </li>
+              <li className="list-group-item position-relative">
+                <p className="row">Cras justo odio</p> 
+                <div className="row tool position-absolute">
+                  <button className="bg-white"><i className="fas fa-pen" /></button>
+                  <button className="bg-primary"><i className="fas fa-check" /></button>
+                  <button className="bg-danger"><i className="fas fa-times" /></button>
+                </div>
+              </li>
+              <li className="list-group-item position-relative">
+                <p className="row">Cras justo odio </p> 
+                <div className="row tool position-absolute">
+                  <button className="bg-white"><i className="fas fa-pen" /></button>
+                  <button className="bg-primary"><i className="fas fa-check" /></button>
+                  <button className="bg-danger"><i className="fas fa-times" /></button>
+                </div>
+              </li>
+              <li className="list-group-item position-relative">
+                <p className="row">Cras justo odio</p>
+                <div className="row tool position-absolute">
+                  <button className="bg-white"><i className="fas fa-pen" /></button>
+                  <button className="bg-primary"><i className="fas fa-check" /></button>
+                  <button className="bg-danger"><i className="fas fa-times" /></button>
+                </div>
+              </li>
+              <li className="list-group-item position-relative">
+                <p className="row">Cras justo odio</p> 
+                <div className="row tool position-absolute">
+                  <button className="bg-white"><i className="fas fa-pen" /></button>
+                  <button className="bg-primary"><i className="fas fa-check" /></button>
+                  <button className="bg-danger"><i className="fas fa-times" /></button>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
